5a7728531f4a4f26e29b1e4fb021e83d37ef19da
Eder Diaz <ederchrono@gmail.com>
2017-04-12 15:48:20 -0500

    Improve header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,38 @@
 import * as React from 'react'
 import Head from 'next/head'
 
-export default () => 
-    <Head>
-        <title>Fox News PoC</title>
-        
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
-        
-        {/*<!-- Latest compiled and minified boostrap CSS -->*/}
-        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css' />
+interface HeaderProps{
+    title?: string
+}
 
-    </Head>
+interface HeaderState{
+    title: string
+}
+
+export default class Header extends React.Component<HeaderProps, HeaderState>  {
+
+    public static defaultProps: Partial<HeaderProps> = {
+        title: "My App"
+    };
+
+    constructor(props : HeaderProps){
+        super(props);
+        // set initial state
+        this.state= { title: this.props.title };
+    }
+
+    public render() {
+        return (
+            <Head>
+                <title>{this.state.title}</title>
+                
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
+                
+                {/*<!-- Latest compiled and minified boostrap CSS -->*/}
+                <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css' />
+
+            </Head>
+        )
+    }
+}
